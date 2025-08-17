0c7b5e69388343a8f2fdaf8dfff6a3dda5ae78e4
Ian Vaserman <ivaserman@frba.utn.edu.ar>
2025-08-17 14:04:57 -0300

    ya funca

--- Logic/logic.js
+++ Logic/logic.js
@@ -1,7 +1,15 @@
 const apiKey = "ed0ad8f86bd7b8e9af5a8176955bac29";
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=london";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+const defaultCity = "London";
+
+searchBtn = document.querySelector(".search-button");
+searchBox = document.querySelector(".search-box");
+
 async function getWeather(city){
-    const response = await fetch( apiUrl + "&appid=" + apiKey + "&q=" + city);
+    if (!city) {
+        city = defaultCity;
+    }
+    const response = await fetch( apiUrl + city + "&appid=" + apiKey );
     var data = await response.json();
     if (data.cod === 200) {
         const weather = {
@@ -15,12 +23,44 @@ async function getWeather(city){
         
         document.getElementById("city-name").innerText = weather.city;
         document.getElementById("temperature").innerText = weather.temperature + "°C";
-        document.getElementById("wind-speed").innerText = weather.wind + " m/s";
+        document.getElementById("wind-speed").innerText = weather.wind + " km/h";
         document.getElementById("humidity").innerText = weather.humidity + "%";
-    }
+        
+        if(data.weather[0].main === "Clouds") {
+            document.getElementById("weather-icon").src = "./images/clouds.png";
+        }
+        else if(data.weather[0].main === "Clear") {
+            document.getElementById("weather-icon").src = "./images/clear.png";
+        }
+        else if(data.weather[0].main === "Rain") {
+            document.getElementById("weather-icon").src = "./images/rain.png";
+        }
+        else if(data.weather[0].main === "Snow") {
+            document.getElementById("weather-icon").src = "./images/snow.png";
+        }
+        else if(data.weather[0].main === "Drizzle") {
+            document.getElementById("weather-icon").src = "./images/drizzle.png";
+        }
+        else if(data.weather[0].main === "Thunderstorm") {
+            document.getElementById("weather-icon").src = "./images/thunderstorm.png";
+        }
+
+        document.getElementById("weather-result").style.display = "block";
+    }   
     else {
-        alert("Error fetching weather data:", data.message);
+        alert("No weather found for the specified city. Please try again.");
     }
 }
 
-getWeather();
+
+searchBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    const cityInput = searchBox.value.trim();
+    if (cityInput) {
+        getWeather(cityInput);
+    } else {
+        getWeather(defaultCity);
+    }
+});
+
+
